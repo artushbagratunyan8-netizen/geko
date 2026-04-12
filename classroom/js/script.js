7eccde58d06afbb6a6e80468f5b8916342b6b2f5
artushbagratunyan8-netizen <artushbagratunyan8@gmail.com>
2026-04-12 20:24:30 +0400

    function

---
+++ classroom/js/script.js
@@ -0,0 +1,5 @@
+/*let nerka = 2026
+function getAge (tvakan){
+    return nerka - tvakan
+}
+console.log(getAge(1999))
---
+++ homework/15-JS_Authorization/script.js
@@ -0,0 +1,27 @@
+const users = [
+  { username: "admin", password: "1234", isBlocked: false },
+  { username: "john", password: "qwerty", isBlocked: true },
+  { username: "anna", password: "pass", isBlocked: false }
+];
+function login(users, username, password) {
+    for(let i = 0; i < users.length; i++) {
+
+        if(users[i].username === username) {
+            if (users[i].password !== password){
+                console.log("Սխալ Գաղտնաբառ");
+            } 
+            else if (users[i].isBlocked) {
+                console.log("Օգտատերը արգելափակված է ");
+            }
+            else{
+                console.log("Բարի գալուստ," + username);
+            }
+            return;
+        }
+    }
+    console.log("Օգտատեռը չի գտնվել");
+}
+login(users, "admin", "1234");
+login(users, "john", "qwerty");
+login(users, "anna", "1111");
+login(users, "mike", "2222");
